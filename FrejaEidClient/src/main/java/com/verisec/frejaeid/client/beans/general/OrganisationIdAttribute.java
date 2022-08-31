76623e6d7106c5cf128b8833a8a783004d7414c0
Isidora Zecevic <isidora.zecevic@frejaeid.rs>
2022-08-31 12:58:11 +0200

    Renamed variable and updated javadoc

--- FrejaEidClient/src/main/java/com/verisec/frejaeid/client/beans/general/OrganisationIdAttribute.java
+++ FrejaEidClient/src/main/java/com/verisec/frejaeid/client/beans/general/OrganisationIdAttribute.java
@@ -8,31 +8,31 @@ import java.util.Objects;
 public class OrganisationIdAttribute {
 
     private final String key;
-    private final String friendlyName;
+    private final String displayText;
     private final String value;
 
     /**
      * Creates instance of {@linkplain OrganisationIdAttribute}.
      *
-     * @param key            identifier of the attribute. It is not displayed to the end user.
-     *                       It cannot be {@code null} or empty.
-     *
-     * @param friendlyName   name of attribute that will be set to user (for
-     *                       example "employee number"). Maximum length is 30 characters. It cannot be
-     *                       {@code null} or empty.
-     * @param value          value of attribute that will be set for user. Maximum length is 128 characters.
-     *                       It cannot be {@code null} or empty.
+     * @param key         identifier of the attribute. It is not displayed to the end user. Maximum length is 64
+     *                    characters. It cannot be {@code null} or empty.
+     * @param displayText name of attribute that will be set to user (for
+     *                    example "employee number"). Maximum length is 64 characters. It cannot be
+     *                    {@code null} or empty.
+     * @param value       value of attribute that will be set for user. Maximum length is 256 characters.
+     *                    It cannot be {@code null} or empty.
      * @return information about organisation id attribute
      */
-    public static OrganisationIdAttribute create(String key, String friendlyName, String value) {
-        return new OrganisationIdAttribute(key, friendlyName, value);
+    public static OrganisationIdAttribute create(String key, String displayText, String value) {
+        return new OrganisationIdAttribute(key, displayText, value);
     }
+
     @JsonCreator
     private OrganisationIdAttribute(@JsonProperty("key") String key,
-                                    @JsonProperty("friendlyName") String friendlyName,
+                                    @JsonProperty("friendlyName") String displayText,
                                     @JsonProperty("value") String value) {
         this.key = key;
-        this.friendlyName = friendlyName;
+        this.displayText = displayText;
         this.value = value;
     }
 
@@ -40,8 +40,8 @@ public class OrganisationIdAttribute {
         return key;
     }
 
-    public String getFriendlyName() {
-        return friendlyName;
+    public String getDisplayText() {
+        return displayText;
     }
 
     public String getValue() {
@@ -53,21 +53,21 @@ public class OrganisationIdAttribute {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         OrganisationIdAttribute that = (OrganisationIdAttribute) o;
-        return Objects.equals(key, that.key) && Objects.equals(friendlyName,
-                                                               that.friendlyName) && Objects.equals(
-                value, that.value);
+        return Objects.equals(key, that.key)
+                && Objects.equals(displayText, that.displayText)
+                && Objects.equals(value, that.value);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(key, friendlyName, value);
+        return Objects.hash(key, displayText, value);
     }
 
     @Override
     public String toString() {
         return "OrganisationIdAttribute{" +
                 "key='" + key + '\'' +
-                ", friendlyName='" + friendlyName + '\'' +
+                ", displayText='" + displayText + '\'' +
                 ", value='" + value + '\'' +
                 '}';
     }
