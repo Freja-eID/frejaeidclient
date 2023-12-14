bf0e124a7328b04fc25b6d9e098cfe171393ffb1
tanjaFrejaEid <tanja.zivkovic@frejaeid.rs>
2023-12-14 13:21:46 +0100

    PR comments - removed unnecessary constructor

--- FrejaEidClient/src/main/java/com/verisec/frejaeid/client/beans/organisationid/update/UpdateOrganisationIdRequest.java
+++ FrejaEidClient/src/main/java/com/verisec/frejaeid/client/beans/organisationid/update/UpdateOrganisationIdRequest.java
@@ -1,7 +1,6 @@
 package com.verisec.frejaeid.client.beans.organisationid.update;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.verisec.frejaeid.client.beans.common.RelyingPartyRequest;
 import com.verisec.frejaeid.client.beans.general.OrganisationIdAttribute;
@@ -15,13 +14,14 @@ public class UpdateOrganisationIdRequest implements RelyingPartyRequest {
     private final List<OrganisationIdAttribute> additionalAttributes;
     private final String relyingPartyId;
 
-    public static UpdateOrganisationIdRequest create(String identifier, List<OrganisationIdAttribute> additionalAttributes){
-        return new UpdateOrganisationIdRequest(identifier, additionalAttributes);
+    public static UpdateOrganisationIdRequest create(String identifier, List<OrganisationIdAttribute> additionalAttributes) {
+        return new UpdateOrganisationIdRequest(identifier, additionalAttributes, null);
     }
 
-    public static UpdateOrganisationIdRequest create(String identifier, List<OrganisationIdAttribute> additionalAttributes, String relyingPartyId){
+    public static UpdateOrganisationIdRequest create(String identifier, List<OrganisationIdAttribute> additionalAttributes, String relyingPartyId) {
         return new UpdateOrganisationIdRequest(identifier, additionalAttributes, relyingPartyId);
     }
+
 
     /**
      * Returns instance of {@linkplain UpdateOrganisationIdRequest}
@@ -30,29 +30,15 @@ public class UpdateOrganisationIdRequest implements RelyingPartyRequest {
      *                             {@code null} or empty.
      * @param additionalAttributes additional attributes related to the identifier.
      *                             It can be {@code null} or empty.
+     * @param relyingPartyId       specifies relying party id for which transaction is
+     *                             initiated. It cannot be {@code null} or empty.
      * @return request
      */
     @JsonCreator
-    private UpdateOrganisationIdRequest(@JsonProperty("identifier") String identifier,
-                                        @JsonProperty("additionalAttributes") List<OrganisationIdAttribute> additionalAttributes) {
-        this.identifier = identifier;
-        this.additionalAttributes = additionalAttributes;
-        this.relyingPartyId = null;
-    }
-
-    /**
-     * Returns instance of {@linkplain UpdateOrganisationIdRequest}
-     *
-     * @param identifier            identifier to be updated for the end user. It cannot be
-     *                              {@code null} or empty.
-     * @param additionalAttributes  additional attributes related to the identifier.
-     *                              It can be {@code null} or empty.
-     * @param relyingPartyId        specifies relying party id for which transaction is
-     *                              initiated. It cannot be {@code null} or empty.
-     * @return request
-     */
-    private UpdateOrganisationIdRequest(String identifier, List<OrganisationIdAttribute> additionalAttributes,
-                                       String relyingPartyId) {
+    private UpdateOrganisationIdRequest(
+            @JsonProperty("identifier") String identifier,
+            @JsonProperty("additionalAttributes") List<OrganisationIdAttribute> additionalAttributes,
+            @JsonProperty("relyingPartyId") String relyingPartyId) {
         this.identifier = identifier;
         this.additionalAttributes = additionalAttributes;
         this.relyingPartyId = relyingPartyId;
